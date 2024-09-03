b691222dff48fa4bfa10df842d61ba61b7dba21c
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-03 12:49:23 +0200

    fix: scroll problem

--- MVPVoteWebsite/src/components/pages/Home/Home.tsx
+++ MVPVoteWebsite/src/components/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
   return (
-  <div className="bg-primary w-screen h-screen grid content-center justify-items-center">
+  <div className="bg-primary w-screen h-screen grid content-center justify-items-center overflow-hidden">
     <button className="bg-primary text-secondary border-4 p-2 w-40 text-2xl font-gill rounded-xl top-6 right-6 absolute">Log in</button>
     <img src="/logo_noBackground.png" alt="" className="h-52"/>
     <h1 className="text-secondary text-4xl mt-4 font-gill font-normal">Vote</h1>
