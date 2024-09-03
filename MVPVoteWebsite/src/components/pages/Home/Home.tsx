ca76b7b549055e21cc86e025feca055f7936ead6
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-03 12:17:42 +0200

    setup complete

---
+++ MVPVoteWebsite/src/components/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+  <div className="bg-primary w-screen h-screen grid content-center justify-items-center">
+    <img src="/logo.svg" alt="" className="h-64"/>
+  </div>
+  );
+};
+
+export default Home;
