8dcbf0299590414abb207368e215a93a4944c40d
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-20 18:59:21 +0200

    using firebase as a control

--- packages/web/src/composables/useGraphql.ts
+++ packages/web/src/composables/useGraphql.ts
@@ -4,27 +4,27 @@ import {
     InMemoryCache,
   } from '@apollo/client/core'
   import { setContext } from '@apollo/client/link/context'
-  // import useFirebase from './useFirebase'
+  import useFirebase from './useFirebase'
   
-  // const { firebaseUser } = useFirebase()
+  const { firebaseUser } = useFirebase()
   
   const httpLink = createHttpLink({
     uri: 'http://[::1]:3000/graphql/',
     credentials: 'same-origin',
   })
   
-  // const authLink = setContext(async (_, { headers }) => ({
-  //   headers: {
-  //     ...headers,
-  //     authorization: firebaseUser.value
-  //       ? `Bearer ${await firebaseUser.value.getIdToken()}`
-  //       : ``,
-  //   },
-  // }))
+  const authLink = setContext(async (_, { headers }) => ({
+    headers: {
+      ...headers,
+      authorization: firebaseUser.value
+        ? `Bearer ${await firebaseUser.value.getIdToken()}`
+        : ``,
+    },
+  }))
   
   const apolloClient = new ApolloClient({
-    // link: authLink.concat(httpLink),
-    link: httpLink,
+    link: authLink.concat(httpLink),
+    // link: httpLink,
     cache: new InMemoryCache(),
   })
   
