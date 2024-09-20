269348a08377e6d324cdd87d6aca3b81872082bc
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-20 19:11:50 +0200

    added firebase guards

--- packages/api/src/vote/vote.resolver.ts
+++ packages/api/src/vote/vote.resolver.ts
@@ -3,11 +3,14 @@ import { VoteService } from './vote.service';
 import { Vote } from './entities/vote.entity';
 import { CreateVoteInput } from './dto/create-vote.input';
 import { CreatePersonalVoteInput } from './dto/create-personal-vote.input';
+import { UseGuards } from '@nestjs/common';
+import { FirebaseGuard } from 'src/authentication/guards/firebase.guard';
 
 @Resolver(() => Vote)
 export class VoteResolver {
   constructor(private readonly voteService: VoteService) {}
 
+  @UseGuards(FirebaseGuard)
   @Mutation(() => Vote, { description: 'Create a new vote' })
   createVote(@Args('createVoteInput') createVoteInput: CreateVoteInput): Promise<Vote> {
     return this.voteService.create(createVoteInput);
@@ -18,11 +21,13 @@ export class VoteResolver {
     return this.voteService.addPersonalVote(personalVoteInput);
   }
 
+  @UseGuards(FirebaseGuard)
   @Mutation(() => Vote, { description: 'Add amount of teammates' })
   addTeammates(@Args('voteId', { type: () => String }) voteId: string, @Args('teammates', { type: () => Int }) teammates: number): Promise<Vote> {
     return this.voteService.addTeammates(voteId, teammates);
   }
 
+  @UseGuards(FirebaseGuard)
   @Mutation(() => Vote, { description: 'Close vote' })
   closeVote(@Args('voteId', { type: () => String }) voteId: string): Promise<Vote> {
     return this.voteService.closeVote(voteId);
@@ -33,16 +38,19 @@ export class VoteResolver {
     return this.voteService.findAll();
   }
 
+  @UseGuards(FirebaseGuard)
   @Query(() => [Vote], { name: 'votesByCreatorUid' })
   votesByCreatorUid(@Args('uid', { type: () => String }) uid: string) {
     return this.voteService.findByCreatorUid(uid);
   }
 
+  @UseGuards(FirebaseGuard)
   @Query(() => Vote, { name: 'voteByVoteId' })
   findByVoteId(@Args('voteId', { type: () => String }) voteId: string) {
     return this.voteService.findByVoteId(voteId);
   }
 
+  @UseGuards(FirebaseGuard)
   @Mutation(() => Vote)
   removeVote(@Args('id', { type: () => Int }) id: number) {
     return this.voteService.remove(id);
