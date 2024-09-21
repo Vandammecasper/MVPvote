4805e787109d6dfa7d96dea0721903c1dc8d997e
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-21 22:02:16 +0200

    removed firebaseAuth

--- packages/web/src/composables/useGraphql.ts
+++ packages/web/src/composables/useGraphql.ts
@@ -3,28 +3,28 @@ import {
     ApolloClient,
     InMemoryCache,
   } from '@apollo/client/core'
-  import { setContext } from '@apollo/client/link/context'
-  import useFirebase from './useFirebase'
+  // import { setContext } from '@apollo/client/link/context'
+  // import useFirebase from './useFirebase'
   
-  const { firebaseUser } = useFirebase()
+  // const { firebaseUser } = useFirebase()
   
   const httpLink = createHttpLink({
     uri: 'https://mvpapi-513k.onrender.com/graphql/',
     credentials: 'include',
   })
   
-  const authLink = setContext(async (_, { headers }) => ({
-    headers: {
-      ...headers,
-      authorization: firebaseUser.value
-        ? `Bearer ${await firebaseUser.value.getIdToken()}`
-        : ``,
-    },
-  }))
+  // const authLink = setContext(async (_, { headers }) => ({
+  //   headers: {
+  //     ...headers,
+  //     authorization: firebaseUser.value
+  //       ? `Bearer ${await firebaseUser.value.getIdToken()}`
+  //       : ``,
+  //   },
+  // }))
   
   const apolloClient = new ApolloClient({
-    link: authLink.concat(httpLink),
-    // link: httpLink,
+    // link: authLink.concat(httpLink),
+    link: httpLink,
     cache: new InMemoryCache(),
   })
   
