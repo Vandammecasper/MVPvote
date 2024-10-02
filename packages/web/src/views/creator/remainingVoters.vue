d796db207522b7b58f06c68667a10e400396f344
Casper Van Damme <casper.van.damme@student.howest.be>
2024-10-02 17:00:22 +0200

    added amount of people that have voted indication on waiting page

--- packages/web/src/views/creator/remainingVoters.vue
+++ packages/web/src/views/creator/remainingVoters.vue
@@ -11,8 +11,10 @@ const { result: voteByVoteIdResult } = useQuery(VOTE_BY_VOTE_ID, {
 
 const { mutate: closeVoteMutation } = useMutation(CLOSE_VOTE)
 
-let remainingVoters = voteByVoteIdResult.value?.vote?.teammates
+let remainingVoters = voteByVoteIdResult.value?.vote?.personalVotes.length()
 const voteId = router.currentRoute.value.params.code
+
+console.log(remainingVoters)
 
 const finishVoting = () => {
     closeVoteMutation({
@@ -29,18 +31,19 @@ const finishVoting = () => {
   <div className="bg-primary w-screen h-screen px-8 flex flex-col items-center">
     <img src="/logo_noBackground.png" alt="" className="h-28 mt-4"/>
     <h1 className="font-gill text-secondary text-center text-3xl mt-2">Your teammates are voting</h1>
-    <!-- <div v-if="remainingVoters > 0" className="mt-48">
-        <h2 className="font-gill text-secondary text-center text-3xl">Still having to vote:</h2>
-        <p className="font-gill text-secondary text-center text-4xl mt-4">{{remainingVoters}}</p>
+    <div className="mt-48">
+        <h2 className="font-gill text-secondary text-center text-3xl">Have voted:</h2>
+        <p className="font-gill text-secondary text-center text-4xl mt-4">{{voteByVoteIdResult.voteByVoteId.personalVotes.length}}</p>
     </div>
-    <div v-else className="mt-48 grid justify-items-center gap-8">
+    <p className="font-gill text-secondary text-xs fixed bottom-24">reload this page to update the number</p>
+    <!-- <div v-else className="mt-48 grid justify-items-center gap-8">
         <h2 className="font-gill text-secondary text-center text-3xl">Everyone has voted</h2>
         <img src="/voted.svg" alt="">
     </div> -->
-    <div className="mt-48 grid justify-items-center gap-8">
+    <!-- <div className="mt-48 grid justify-items-center gap-8">
         <h2 className="font-gill text-secondary text-center text-3xl">Ask whether everyone has voted</h2>
         <img src="/voted.svg" alt="">
-    </div>
+    </div> -->
     <!-- <button @click="() => finishVoting()" v-if="remainingVoters > 0" className="justify-self-center text-secondary border-4 border-secondary text-3xl font-gill fixed bottom-0 w-screen py-4">Finish voting</button> -->
     <button @click="() => finishVoting()" className="bg-secondary justify-self-center text-primary text-3xl font-gill fixed bottom-0 w-screen py-4">Go to results</button>
   </div>
