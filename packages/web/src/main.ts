831ab37a56a7aaef63a82cd76af70b8827a025aa
Casper Van Damme <casper.van.damme@student.howest.be>
2024-10-04 10:27:18 +0200

    added restore use to main

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -3,11 +3,13 @@ import './index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './bootstrap/router'
+import useFirebase from './composables/useFirebase'
 
 const app = createApp(App)
+const { restoreUser } = useFirebase()
 
 ;(async () => {
-    // await restoreUser()
+    await restoreUser()
   
     app.use(router)
     app.mount('#app')
